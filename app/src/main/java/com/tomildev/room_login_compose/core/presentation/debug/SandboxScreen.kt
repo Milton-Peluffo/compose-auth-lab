95ce51ddf46fb86db3b205828ee2cc88ba29304f
Milton-Peluffo <milton.peluffoc@gmail.com>
2026-01-31 21:52:33 -0500

    feat(debug): add SabdboxScreen for component prototyping

---
+++ app/src/main/java/com/tomildev/room_login_compose/core/presentation/debug/SandboxScreen.kt
@@ -0,0 +1,9 @@
+package com.tomildev.room_login_compose.core.presentation.debug
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SandboxScreen(modifier: Modifier = Modifier) {
+    
+}
